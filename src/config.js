f1b6f71895dc0b4bee52bb28d101a4acf94cc14f
User <user@example.com>
2026-05-22 10:24:51 +0530

    edit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 // Centralized Configuration
 // Edit this URL once to update the entire application
 export const API_BASE_URL = 'http://localhost:5000/api'; 
-//export const API_BASE_URL = 'https://asset-backend-4bz5.onrender.com/api'; 
